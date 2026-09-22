9712f3518be9a0e2ed745ccb318ae78be284fe89
BornaBartolic <101433904+BornaBartolic@users.noreply.github.com>
2026-09-22 23:01:33 +0200

    Update WeatherAPI.kt

--- app/src/main/java/hr/algebra/weatherapp/api/WeatherAPI.kt
+++ app/src/main/java/hr/algebra/weatherapp/api/WeatherAPI.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 const val API_URL = "https://api.openweathermap.org/"
-const val API_KEY = "b3860da545ed65d5e7eb7dbe20a3fce4"
+const val API_KEY = "API_KEY"
 
 interface WeatherAPI {
     @GET("data/2.5/weather")
